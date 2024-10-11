ddf9582e20778f3534f5a2533261aaf74b749b2f
ajaykumar-21 <ajaykumarpk789@gmail.com>
2024-10-12 01:14:52 +0530

    create social section and apply some css

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,6 +1,14 @@
 import Image from "next/image";
 import styles from "./Footer.module.css";
 import Flag from "../../assets/usaFlag.svg";
+import Insta from "../../assets/Insta.svg";
+import Linkd from "../../assets/linkdIn.svg";
+import Gpay from "../../assets/gpay.svg";
+import Visa from "../../assets/visa.svg";
+import Rupay from "../../assets/rupay.svg";
+import American from "../../assets/american.svg";
+import Apple from "../../assets/applePay.svg";
+import Zero from "../../assets/zeroPay.svg";
 
 function Footer() {
   return (
@@ -22,8 +30,12 @@ function Footer() {
         </div>
         <div>
           <div className={styles.contactUS}>CONTACT US</div>
-          <p style={{ marginBottom: "10px" }}>+44 221 133 5360</p>
-          <p style={{ marginBottom: "10px" }}>customercare@mettamuse.com</p>
+          <p style={{ marginBottom: "10px", opacity: ".7" }}>
+            +44 221 133 5360
+          </p>
+          <p style={{ marginBottom: "10px", opacity: ".7" }}>
+            customercare@mettamuse.com
+          </p>
           <div className={styles.currency}>CURRENCY</div>
           <div className={styles.flagWrapper}>
             <div>
@@ -40,6 +52,77 @@ function Footer() {
       </div>
       <div className={styles.dividerContainer}>
         <div className={styles.divider}></div>
+      </div>
+      <div className={styles.socialSection}>
+        <div>
+          <h2 style={{ marginBottom: "15px" }}>mettā muse</h2>
+          <p style={{ marginBottom: "10px", opacity: ".7" }}>About Us</p>
+          <p style={{ marginBottom: "10px", opacity: ".7" }}>Stories</p>
+          <p style={{ marginBottom: "10px", opacity: ".7" }}>Artisans</p>
+          <p style={{ marginBottom: "10px", opacity: ".7" }}>Boutiques</p>
+          <p style={{ marginBottom: "10px", opacity: ".7" }}>Contact Us</p>
+          <p style={{ marginBottom: "10px", opacity: ".7" }}>
+            EU Compliances Docs
+          </p>
+        </div>
+        <div>
+          <h3 style={{ marginBottom: "15px" }}>QUICK LINKS</h3>
+          <p style={{ marginBottom: "10px", opacity: ".7" }}>
+            Orders & Shipping
+          </p>
+          <p style={{ marginBottom: "10px", opacity: ".7" }}>
+            Join/Login as a Seller
+          </p>
+          <p style={{ marginBottom: "10px", opacity: ".7" }}>
+            Payment & Pricing
+          </p>
+          <p style={{ marginBottom: "10px", opacity: ".7" }}>
+            Return & Refunds
+          </p>
+          <p style={{ marginBottom: "10px", opacity: ".7" }}>FAQs</p>
+          <p style={{ marginBottom: "10px", opacity: ".7" }}>Privacy Policy</p>
+          <p style={{ marginBottom: "10px", opacity: ".7" }}>
+            Terms & Conditions
+          </p>
+        </div>
+        <div>
+          <h3>FOLLOW US</h3>
+          <div className={styles.socialIcon}>
+            <div>
+              <Image src={Insta} alt="Insta icon" />
+            </div>
+            <div>
+              {" "}
+              <Image src={Linkd} alt="Linkd icon" />
+            </div>
+          </div>
+          <h4 style={{ marginTop: "50px", opacity: ".9" }}>
+            mettā muse ACCEPTS
+          </h4>
+          <div className={styles.cardWrapper}>
+            <div>
+              <Image src={Gpay} alt="Gpay Icon" />
+            </div>
+            <div>
+              <Image src={Visa} alt="Visa Icon" />
+            </div>
+            <div>
+              <Image src={Rupay} alt="Rupay Icon" />
+            </div>
+            <div>
+              <Image src={American} alt="American Icon" />
+            </div>
+            <div>
+              <Image src={Apple} alt="Apple Icon" />
+            </div>
+            <div>
+              <Image src={Zero} alt="Zero Icon" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.copyright}>
+        <p>Copyright © 2023 mettamuse. All rights reserved.</p>
       </div>
     </div>
   );
